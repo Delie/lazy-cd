13998cc2f6ffb3968716760a9dc62b8ef7c713ae
Andrew Delicata <Delie@users.noreply.github.com>
2016-07-07 20:11:04 +0100

    Update z.bat

--- z.bat
+++ z.bat
@@ -1,38 +1,47 @@
-@echo off
-REM lazy-cd
-REM https://github.com/Delie/lazy-cd/
-REM Windows batch file to aid quick & lazy directory switching!
-REM Created by Andrew Delicata - https://github.com/Delie
+@ECHO off
+REM  lazy-cd
+REM  https://github.com/Delie/lazy-cd/
+REM  Windows batch file to aid quick & lazy directory switching!
+REM  Written by Andrew Delicata - https://github.com/Delie
 
-set "targetDir="
-setlocal enabledelayedexpansion
-set v1=%1
-set v2=%2
-set /a index = 0
+SET "targetDir="
+SETLOCAL enabledelayedexpansion
+SET v1=%1
+SET v2=%2
+SET /a index = 0
 
-if [%1] == [] (
-    dir /o:g /o:n
-    goto :eof
+IF "%1%." =="." (
+    ENDLOCAL
+    DIR /o:g /o:n
+    GOTO :eof
 )
-if [%2] == [] set /a %v2 = 1
-
-for /d %%f in (%v1%*) do (
-    set /a index = index + 1
-
-    if !index! equ !v2! (
-       rem @echo TARGET DIRECTORY: %%f
-       endlocal
+IF %1 == \ (
+    ENDLOCAL
+    CD\
+    GOTO :eof
+)
+IF %1 == .. (
+    ENDLOCAL
+    CD..
+    GOTO :eof
+)
+IF "%2." =="." SET /a %v2 = 1
+FOR /d %%f in (%v1%*) do (
+    SET /a index = index + 1
+    IF !index! equ !v2! (
+       ENDLOCAL
        SET targetDir=%%f
    )
 )
-endlocal
-
-if "%targetDir%." =="." (
-    echo.
-    echo No directories matched: %1
-    goto :eof
+ENDLOCAL
+IF "%targetDir%." == "." (
+    ECHO.
+    ECHO No directories matched: %1
+    GOTO :eof
 )
-echo Opening child directory: "%targetDir%"
-cd %targetDir%
+CD %targetDir%
+
+REM Uncomment the line below to perform a new DIR after changing directory
+REM DIR /o:g /o:n
 
 :eof
